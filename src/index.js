5e2091e48c72a2cff172c828c3de698effe6d910
EMILS <emils.bisenieks93@gmail.com>
2021-09-22 15:27:17 +0300

    check

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AppRouter from "./router/AppRouter";
-// Importing the Bootstrap CSS
-// import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.render(<AppRouter />, document.querySelector("#root"));
